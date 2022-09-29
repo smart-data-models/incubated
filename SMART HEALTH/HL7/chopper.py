c0c7634fc9cd7be3a5d8b2b9d08ed8e045a1bf00
aabella <alberto.abella@fiware.org>
2022-09-29 20:34:31 +0200

    hl7

---
+++ SMART HEALTH/HL7/chopper.py
@@ -0,0 +1,142 @@
+import copy
+import pathlib
+import json
+
+
+def open_json(fileUrl):
+    import json
+    import requests
+    if fileUrl[0:4] == "http":
+        # es URL
+        try:
+            pointer = requests.get(fileUrl)
+            return json.loads(pointer.content.decode('utf-8'))
+        except:
+            return None
+
+    else:
+        # es file
+        try:
+            file = open(fileUrl, "r")
+            return json.loads(file.read())
+        except:
+            return None
+
+
+def extensor(object_dict, definitions, level):
+    notExpandable = ["ResourceList"]
+    import copy
+    limitDepth = 6
+    newObject = copy.deepcopy(object_dict)
+    for key, value in object_dict.items():
+        print(str(key) + '->' + str(value))
+        if (key == "$ref") and (level < limitDepth):
+            print("_______________________")
+            print("This should be replaced by")
+            term = str(value).replace("#/definitions/", "")
+            if term not in notExpandable:
+                del newObject["$ref"]
+                definition = definitions[term]
+                if "description" in definition:
+                    del definition["description"]
+                print(definition)
+                print("extended $ref =" + str(definition))
+                newObject = dict(newObject, **definition)
+            else:
+                newObject["type"] = "string"
+                del newObject["$ref"]
+
+        # recursivity is limited
+        if key == "$ref" and level >= limitDepth:
+            del newObject["$ref"]
+            if "type" not in newObject:
+                newObject["type"] = "string"
+        if isinstance(value, dict):
+            print("Entering into level " + str(level + 1))
+            newObject[key] = extensor(value, definitions, level + 1)
+        elif isinstance(value, list):
+            for val in value:
+                if isinstance(val, str):
+                    pass
+                elif isinstance(val, list):
+                    pass
+                else:
+                    newObject[key][val] = extensor(newObject[key][val], definitions, level + 1)
+    return newObject
+
+
+schemaHeader = {
+    "$schema": "http://json-schema.org/schema#",
+    "$schemaVersion": "0.0.1",
+    "$id": "https://smart-data-models.github.io/dataModel.HL7/XXXX/schema.json",
+    "title": "Smart Data Models - HL7 / ",
+    "description": "",
+    "modelTags": "HL7",
+    "derivedFrom": "HL7",
+    "license": "",
+    "type": "object",
+    "required": ["id", "type"],
+    "allOf": [
+        {
+            "$ref": "https://smart-data-models.github.io/data-models/common-schema.json#/definitions/GSMA-Commons"
+        },
+        {
+            "$ref": "https://smart-data-models.github.io/data-models/common-schema.json#/definitions/Location-Commons"
+        }
+    ]
+}
+
+filename = "overall_schema.json"
+overallDict = open_json(filename)
+exceptions = ["ResourceList", "base64Binary"]
+definitions = overallDict["definitions"]
+listToParse = overallDict["discriminator"]["mapping"].keys()
+mainlimit = 1000
+maincounter = 0
+for item in listToParse:
+    if (item not in exceptions) and (item in definitions):
+        if maincounter > mainlimit:
+            break
+        else:
+            maincounter += 1
+        exportfilename = "./" + item + "/" + item + ".json"
+        path = pathlib.Path(item)
+        path.mkdir(parents=True, exist_ok=True)
+        with open(exportfilename, "w+") as exportfile:
+            json.dump({item: definitions[item]}, exportfile)
+        # now load the export
+        objectDict = open_json(exportfilename)
+        counter = 0
+        limit = 3
+        while str(objectDict).find("#/definitions/") > -1:
+            counter += 1
+            if counter > limit:
+                break
+            outfilename = "./" + item + "/" + item + "_processed" + str(counter) + ".json"
+            outDict = extensor(objectDict, definitions, 0)
+            print(outDict)
+            with open(outfilename, "w") as outfile:
+                json.dump(outDict, outfile)
+            objectDict = outDict
+        finalfilename = "./" + item + "/" + item + "_processed3.json"
+        preschema = open_json(finalfilename)
+        properties = preschema[item]["properties"]
+        if "type" not in properties:
+            properties["type"] = {
+                "type": "string",
+                "description": "Property. NGSI entity type. It has to be " + item,
+                "enum": [item]
+            }
+        for prop in properties:
+            if "description" in properties[prop]:
+                properties[prop]["description"] = "Property. " + properties[prop]["description"]
+            else:
+                properties[prop]["description"] = "Property. Not described in HL7 original"
+        description = preschema[item]["description"]
+        finalSchema = copy.deepcopy(schemaHeader)
+        finalSchema["$id"] = finalSchema["$id"].replace("XXXX", item)
+        finalSchema["description"] = description
+        finalSchema["title"] += item + " data model based on HL7 equivalent"
+        finalSchema["allOf"].append({"properties": properties})
+        with open("./" + item + "/" + "schema.json", "w+") as outfile:
+            json.dump(finalSchema, outfile)
--- SMART HEALTH/HL7/hl7_extractor.py
+++ SMART HEALTH/HL7/hl7_extractor.py
@@ -1,5 +1,5 @@
 import jsonref
-from copy import deepcopy
+import copy
 
 def open_json(fileUrl):
     import json
@@ -45,26 +45,38 @@ extendedSchema = []
 with open(full_schema_file_name, "r") as file_content:
     schema_txt = file_content.read()
 # print(schema_txt)
-limit = 1
+limit = 0
 counter = 0
-directives = ["additionalProperties"]
+directives = ["additionalProperties", "required"]
 fullDict = jsonref.loads(schema_txt)
-for key in fullDict["definitions"]:
+definitions = fullDict["definitions"]
+# looping across the elements of the definitions, only
+for key in definitions:
     print(key)
     if key != "ResourceList":
-        newConcept = deepcopy(fullDict["definitions"][key])
-        for element in newConcept:
-            print(element)
-            if element not in directives:
-                if "$ref" in newConcept[element]:
-                    if counter > limit:
-                        break
-                    else:
-                        counter += 1
-                    term = newConcept[element]["$ref"].replace("#/definitions/","")
-                    newConcept = {newConcept, fullDict["definitions"][term]}
-                    del newConcept["$ref"]
-        extendedSchema.append(newConcept)
+        # a copy of each element is made, supposedly a dict
+        if "properties" in definitions[key]:
+            if counter > limit:
+                break
+            else:
+                counter += 1
+            newObject = copy.deepcopy(definitions[key]["properties"])
+            print("NewObject")
+            print(newObject)
+            print(type(newObject))
+            for element in newObject:
+                print(key + ": has a subelement " + element)
+                if "$ref" in newObject[element]:
+
+                    # if found it is replaced
+                    # Adding the new reference
+                    term = newObject[element]["$ref"].replace("#/definitions/", "")
+                    newObject = {newObject, definitions[term]}
+                    print("replacing $ref=" + newObject[element]["$ref"] + " with " + str(definitions[term]))
+                    # extending with the relative link
+                    # removing the old one
+                    del newObject["$ref"]
+        extendedSchema.append(newObject)
 print(extendedSchema)
 
 
