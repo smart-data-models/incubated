6423e8f61b84eb8aca3cc4d8a3048a753df94d02
aabella <alberto.abella@fiware.org>
2023-11-02 15:17:31 +0100

    train ontology scripts

--- SMARTLOGISTICS/TRAINS/parser_trains.py
+++ SMARTLOGISTICS/TRAINS/parser_trains.py
@@ -1,17 +1,80 @@
-from rdflib import Graph
+import os
+import json
+import xml.etree.ElementTree as ET
 
-# Read the RDF/XML file into a string variable
-with open('one_property.rdf', 'r') as f:
-    rdf_string = f.read()
+mytree = ET.parse('ontology.rdf')
+myroot = mytree.getroot()
+schemas = {}
 
-# Parse the RDF/XML string into an RDF graph
-g = Graph().parse(data=rdf_string, format='xml')
+for element in myroot:
+    # print(element.tag)
+    # detecting the class elements (entities to be)
 
-# Extract the XML structures you are interested in
-xml_dict = {}
-for s, p, o in g.triples((None, None, None)):
-    if str(p).endswith('ObjectProperty'):
-        xml_dict[str(s)] = str(g.serialize(format='xml', destination=None, source=s))
+    if element.tag == "{http://www.w3.org/2002/07/owl#}Class":
+        print(element.attrib)
+        dataModel = list((element.attrib).values())[0].split("/")[-1]
+        print(dataModel)
+        # a = input("stop")
+        print("_________________________________")
+        sentence = element.attrib
+        _keys = list(sentence.keys())
+        uri = sentence[_keys[0]]
+        derivedFrom = uri
 
-# Print the resulting dictionary
-print(xml_dict)
+        title = ""
+        description = ""
+        for item in element:
+            print(item)
+            title = dataModel + " + mapped from ERA ontology by Smart Data Models"
+            if item.tag == "{http://www.w3.org/2000/01/rdf-schema#}comment":
+                description = item.text
+        print("URI:" + uri)
+        print("Datamodel:" + dataModel)
+        print("Description:" + description)
+        if dataModel != "":
+            schemas[dataModel] = {}
+            schemas[dataModel]["uri"] = uri
+            schemas[dataModel]["title"] = title
+            schemas[dataModel]["description"] = description
+            schemas[dataModel]["derivedFrom"] = derivedFrom
+
+print(schemas)
+with open("output.json", "w") as file:
+    json.dump(schemas, file)
+
+header = {
+    "$schema": "http://json-schema.org/schema#",
+    "$schemaVersion": "0.0.1",
+    "$id": "https://smart-data-models.github.io/dataModel.ERA/XXXX/schema.json",
+    "modelTags": "ERA vocabulary, railway, train",
+    "license": "https://smart-data-models.github.io/dataModel.ERA/XXXX/LICENSE.md",
+    "type": "object",
+    "allOf": [
+        {
+            "$ref": "https://smart-data-models.github.io/data-models/common-schema.json#/definitions/GSMA-Commons"
+        },
+        {
+            "$ref": "https://smart-data-models.github.io/data-models/common-schema.json#/definitions/Location-Commons"
+        },
+        {
+            "properties": {}
+        }
+    ],
+    "required": ["id", "type"]
+}
+print("__________________")
+outputDirectory = "./dataModel.ERA"
+for item in list(schemas.keys()):
+    print(item)
+    schema_object = header
+    schema_object["$id"] = schema_object["$id"].replace("XXXX", item)
+    schema_object["license"] = schema_object["license"].replace("XXXX", item)
+    schema_object["title"] = schemas[item]["title"]
+    schema_object["derivedFrom"] = schemas[item]["derivedFrom"]
+    schema_object["description"] = schemas[item]["description"]
+    dirname = outputDirectory + "/" + item
+    os.makedirs(dirname, exist_ok=True)
+    filename = outputDirectory + "/" + item + "/schema.json"
+    with open(filename, "w") as file:
+        json.dump(schema_object, file)
+
---
+++ SMARTLOGISTICS/TRAINS/ranges.py
@@ -0,0 +1,18 @@
+import os
+import json
+import xml.etree.ElementTree as ET
+
+mytree = ET.parse('ontology.rdf')
+myroot = mytree.getroot()
+
+datatypes = set()
+for element in myroot:
+    if element.tag == "{http://www.w3.org/2002/07/owl#}DatatypeProperty":
+        for item in element:
+            print(item)
+            print(item.tag)
+            print(item.attrib)
+            if item.tag == "{http://www.w3.org/2000/01/rdf-schema#}range":
+                datatypes.add(list((item.attrib).values())[0])
+print("______________________________")
+print(datatypes)
